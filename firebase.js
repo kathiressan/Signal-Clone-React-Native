077158f0eb958939601a000e4d31090031637728
unknown <kathiressan26pro@gmail.com>
2021-09-05 18:18:38 +0800

    First commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6qQucv4E0S5WLpGEFihZunClXeNbfVz4",
+  authDomain: "signal-clone-71f4a.firebaseapp.com",
+  projectId: "signal-clone-71f4a",
+  storageBucket: "signal-clone-71f4a.appspot.com",
+  messagingSenderId: "391992565437",
+  appId: "1:391992565437:web:04eba36388eb7266f521d4",
+};
